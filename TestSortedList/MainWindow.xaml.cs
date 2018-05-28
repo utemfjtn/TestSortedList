d3597e366901648fc95f1ddc909da407d3bafaef
YanQian <utemfjtn@163.com>
2018-05-28 09:58:30 +0800

    C# Test SortedList.

--- TestSortedList/MainWindow.xaml.cs
+++ TestSortedList/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,15 +15,43 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TestSortedList
-{
+{  
+
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SortedList sl = new SortedList();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            sl.Add(DateTime.Now, DateTime.Now);
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            if (sl != null && sl.Count >0 )
+            {
+                // 获取键的集合 
+                ICollection key = sl.Keys;
+
+                Recheck:
+                foreach (DateTime k in key)
+                {
+                    Console.WriteLine(k + ": " + sl[k]);
+                    if ((DateTime) sl[k] < DateTime.Now.AddSeconds(-10))
+                    {
+                        sl.Remove(k);
+                        goto Recheck;
+                    }
+                }
+            }
         }
     }
 }
